d12a630338e7a642460cfb0e973a916256dd236d
Guilherme Jesus <g_bernardo10@hotmail.com>
2020-05-01 04:03:15 -0300

    feat: add code report test coverage

--- user-service/__tests__/UserController.test.js
+++ user-service/__tests__/UserController.test.js
@@ -1,7 +1,12 @@
 import request from 'supertest';
 import { app } from '../src/server';
+import User from '../src/models/User';
 
 describe('User', () => {
+  beforeEach(async () => {
+    const qtd = await User.count();
+    if (qtd > 0) await User.destroy({ truncate: true });
+  });
   it('should create user', async () => {
     const user = {
       name: 'Guilherme Jesus',
@@ -12,9 +17,20 @@ describe('User', () => {
     const response = await request(app)
       .post('/users')
       .send(user);
-
-    console.log(response.status);
-
     expect(response.status).toBe(201);
+  });
+
+  it('received status 400, missing required field username ', async () => {
+    const user = {
+      name: 'Guilherme Jesus',
+      username: '',
+      mail: 'jesus@email.com',
+      password: 'senha-segura',
+    };
+
+    const response = await request(app)
+      .post('/users')
+      .send(user);
+    expect(response.status).toBe(400);
   });
 });
