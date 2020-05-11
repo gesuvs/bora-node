999acd84e1cb4bc1df3aefa5f1eca8d5a715ad32
GBernardo10 <g_bernardo10@hotmail.com>
2020-05-11 03:49:02 -0300

    chore: add travis 7

--- ec2-setup.sh
+++ ec2-setup.sh
@@ -1,2 +1,5 @@
-#ssh -i "ec2-teste.pem" ec2-user@ec2-18-206-223-123.compute-1.amazonaws.com
-docker-machine ssh master && ls && exit
+sleep 3s
+ssh -i "ec2-teste.pem" ec2-user@ec2-18-206-223-123.compute-1.amazonaws.com
+sleep 3s
+sudo yum update
+exit
