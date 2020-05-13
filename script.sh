05e2aae04a5390635da46910dd52218164ad8342
GBernardo10 <g_bernardo10@hotmail.com>
2020-05-13 16:35:20 -0300

    deploy: rm log

--- script.sh
+++ script.sh
@@ -33,6 +33,20 @@ status() {
   done
 }
 
+rmlogpush() {
+  git config --global user.email "g_bernardo10@hotmail.com"
+  git config --global user.name "GBernardo10"
+  echo "Host github.com" >~/.ssh/config
+  echo "  IdentityFile  $(pwd)/travis_key" >>~/.ssh/config
+  chmod 400 travis_key
+  echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >~/.ssh/known_hosts
+  git remote set-url origin git@github.com:GBernardo10/bora-node.git
+  git checkout feature/lerna
+  git add .log
+  git commit -m "deploy: rm log"
+  git push origin feature/lerna --quiet
+}
+
 arquivo() {
   if [ -f $FILE ]; then
     if [[ -z $(grep '[^[:space:]]' $FILE) ]]; then
