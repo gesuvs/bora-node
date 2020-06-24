ac4fcc468654d6ca27b33f58342c9e8f35f518c9
GBernardo10 <g_bernardo10@hotmail.com>
2020-06-24 18:17:36 -0300

    remove: routes healthcheck wrong implementation

--- eventos-service/src/routes/healthcheck.ts
+++
@@ -1,7 +0,0 @@
-import { Response, Request } from 'express';
-
-export const healthcheck = (req: Request, res: Response): Response<string> => {
-  return res.json({
-    healthcheck: 'ok',
-  });
-};
